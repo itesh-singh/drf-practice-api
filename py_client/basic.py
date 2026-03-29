86c04846e4b42f0bfc0f763f9f14170ccdbbea69
itesh-singh <itesh5906@gmail.com>
2026-03-29 07:40:48 +0530

    feat(api): add GET endpoint with echo response

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/" # http://localhost:8000/api/
 
 get_response = requests.get(endpoint) # HTTP request
 print(get_response.text) # print the raw response text
@@ -12,5 +12,5 @@ print(get_response.status_code)
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation (JSON) -> Python Dictionary
-# print(get_response.json()) # print the response as a Python dictionary
+print(get_response.json()['message']) # print the response as a Python dictionary
 # print(get_response.status_code) # print the HTTP status code of the response
