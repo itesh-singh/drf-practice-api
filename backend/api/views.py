bff6ae32b85b13e3781e54183981ec26498ab1c7
itesh-singh <itesh5906@gmail.com>
2026-03-30 08:16:09 +0530

    feat(models): create model and return data in API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,19 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Strongest of Today."})
+    print(request.GET)
+    print(request.POST)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    # data['headers'] = dict(request.headers)
+    data['params'] = dict(request.GET)
+    data['headers'] = (dict(request.headers))
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,13 +4,13 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" # http://localhost:8000/api/
 
-get_response = requests.get(endpoint) # HTTP request
-print(get_response.text) # print the raw response text
-print(get_response.status_code)
+get_response = requests.get(endpoint, params={'abc': 123}, json={'message': 'hello world'}) # HTTP request
+# print(get_response.text) # print the raw response text
+# print(get_response.status_code)
 
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation (JSON) -> Python Dictionary
-print(get_response.json()['message']) # print the response as a Python dictionary
+print(get_response.json()) # print the response as a Python dictionary
 # print(get_response.status_code) # print the HTTP status code of the response
