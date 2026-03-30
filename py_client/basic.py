46ebca6d134f3ece8ff3571dd02f5e46d370be07
itesh-singh <itesh5906@gmail.com>
2026-03-30 09:54:44 +0530

    refactor(api): use DRF APIView and Response

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,8 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" # http://localhost:8000/api/
 
-get_response = requests.get(endpoint, params={'abc': 123}, json={'message': 'hello world'}) # HTTP request
+get_response = requests.get(endpoint, json={"product_id": 123}) # HTTP request
+# print(get_response.headers) # print the response headers
 # print(get_response.text) # print the raw response text
 # print(get_response.status_code)
 
