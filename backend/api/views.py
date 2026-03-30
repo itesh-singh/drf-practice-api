d71522808170eef5856b777199358dc582c7956f
itesh-singh <itesh5906@gmail.com>
2026-03-30 08:44:00 +0530

    feat(api): convert model to dictionary response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,13 @@
 import json
 from django.http import JsonResponse
-
+from products.models import Product 
 
 def api_home(request, *args, **kwargs):
-    print(request.GET)
-    print(request.POST)
-    body = request.body
+    model_data = Product.objects.all().order_by('?').first() # get a random product from the database 
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    # data['headers'] = dict(request.headers)
-    data['params'] = dict(request.GET)
-    data['headers'] = (dict(request.headers))
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
