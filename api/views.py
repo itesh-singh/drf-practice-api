29df4e3950f2efb80fc18fab94b0615cb0a3efcb
itesh-singh <itesh5906@gmail.com>
2026-04-04 07:40:41 +0530

    feat(fbv): add function-based GET endpoint for student data

--- api/views.py
+++ api/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
+@api_view(['GET'])
 def studentsView(request):
-    students = Student.objects.all()
-    students_list = list(students.values())
-    return JsonResponse(students_list, safe=False)
+    if request.method == 'GET':
+        # Get all students from the database
+        students = Student.objects.all()
+        # Serialize the students data
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
